69805177bfa50da8e424d432bb8a9f0f9447e444
voreiche <volker@vritb.de>
2015-04-17 19:08:54 +0200

    entpacken

---
+++ src/main/java/de/vrsoft/springboot/demo/kontaktliste/KontaktlisteApplication.java
@@ -0,0 +1,12 @@
+package de.vrsoft.springboot.demo.kontaktliste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KontaktlisteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KontaktlisteApplication.class, args);
+    }
+}
---
+++ src/test/java/de/vrsoft/springboot/demo/kontaktliste/KontaktlisteApplicationTests.java
@@ -0,0 +1,18 @@
+package de.vrsoft.springboot.demo.kontaktliste;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = KontaktlisteApplication.class)
+@WebAppConfiguration
+public class KontaktlisteApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
